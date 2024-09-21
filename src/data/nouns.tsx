154564f1e6845e9fd704cb3cd7e8d7834f89cb2f
zexoverz <faisalfirdani01@gmail.com>
2024-09-21 09:36:38 +0800

    Update Dashboard NFT

---
+++ src/data/nouns.tsx
@@ -0,0 +1,12 @@
+export const nounsicon = [
+    {
+        id: 1,
+        imageSrc: "https://ibb.co/f1RCcym",
+        title: "Marriage",
+    },
+    {
+        id: 1,
+        imageSrc: "https://ibb.co/0j3Jz5M",
+        title: "Earth",
+    },
+]
