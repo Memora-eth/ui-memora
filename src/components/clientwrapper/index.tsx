f4f637786fd21a5b9982423ef3f62d4ff6c13bcd
Verestra <revoparker@gmail.com>
2024-09-21 09:59:28 +0800

    feat: farcaster profile integration

--- src/components/clientwrapper/index.tsx
+++ src/components/clientwrapper/index.tsx
@@ -2,43 +2,50 @@
 "use client";
 
 import { AuthProvider } from "@/context/AuthContext";
+import { FarcasterProvider } from "@/context/FarcasterContext";
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import Headers from "@/components/headers";
 import DarkMode from "@/components/common/DarkMode";
 
 const evmNetworks = [
-    {
-      blockExplorerUrls: ['https://explorer.testnet.rsk.co'],
-      chainId: 31,
-      chainName: 'RSK Testnet',
-      iconUrls: ['https://chainlist.org/unknown-logo.png'],
-      name: 'RSK',
-      nativeCurrency: {
-        name: 'RSK Smart Bitcoin',
-        symbol: 'tRBTC',
-        decimals: 18,
-      },
-      networkId: 31,
-      rpcUrls: ['https://public-node.testnet.rsk.co'],
-      vanityName: 'RSK Testnet',
+  {
+    blockExplorerUrls: ["https://explorer.testnet.rsk.co"],
+    chainId: 31,
+    chainName: "RSK Testnet",
+    iconUrls: ["https://chainlist.org/unknown-logo.png"],
+    name: "RSK",
+    nativeCurrency: {
+      name: "RSK Smart Bitcoin",
+      symbol: "tRBTC",
+      decimals: 18,
     },
-  ];
+    networkId: 31,
+    rpcUrls: ["https://public-node.testnet.rsk.co"],
+    vanityName: "RSK Testnet",
+  },
+];
 
-export default function ClientWrapper({ children }: { children: React.ReactNode }) {
+export default function ClientWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID || "",
+        walletConnectors: [EthereumWalletConnectors],
+        overrides: { evmNetworks },
+      }}
+    >
     <AuthProvider>
-      <DynamicContextProvider
-        settings={{
-          environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID || "",
-          walletConnectors: [EthereumWalletConnectors],
-          overrides: { evmNetworks },
-        }}
-      >
-        <DarkMode />
-        <Headers />
-        {children}
-      </DynamicContextProvider>
+      <FarcasterProvider>
+          <DarkMode />
+          <Headers />
+          {children}
+      </FarcasterProvider>
     </AuthProvider>
+    </DynamicContextProvider>
   );
 }
