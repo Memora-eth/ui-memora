744f999d6c1f75234ff85dece0a5e191fe1223b0
Verestra <revoparker@gmail.com>
2024-09-21 10:31:19 +0800

    patch: dashboard management

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,21 +18,8 @@ export default function Dashboard() {
       <Headers />
       <main className="pt-[5.5rem] lg:pt-48">
         <Profile />
-
       <Collections />
       </main>
-
-      <CreateAction/>
-      
-      <section className=" flex justify-center h-64 rounded-lg mt-20">
-          <div className="flex flex-row pt-10 gap-2">
-            <h1 className=" cursor-pointer hover:text-white h-10 bg-jacarta-700 rounded-lg p-2">Linked Account</h1>
-            <h1 className=" cursor-pointer hover:text-white h-10 bg-jacarta-700 rounded-lg p-2">NFT Status</h1>
-            <h1 className=" cursor-pointer hover:text-white h-10 bg-jacarta-700 rounded-lg p-2">Heir Status</h1>
-            <h1 className=" cursor-pointer hover:text-white h-10 bg-jacarta-700 rounded-lg p-2">Recent Activity</h1>
-          </div>
-      </section>
-
       <Footers />
     </>
   );
--- src/components/dashboard/collections.tsx
+++ src/components/dashboard/collections.tsx
@@ -1,15 +1,24 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import tippy from "tippy.js";
-
-import { collections } from "@/data/collections";
 import Link from "next/link";
+import tippy from "tippy.js";
+import CreateAction from "./CreateAction";
 import Image from "next/image";
+import { collections } from "@/data/collections";
+
+type TabType = "owned" | "herited" | "create";
 
 export default function Collections() {
+  const [activeTab, setActiveTab] = useState<TabType>("owned");
+
   useEffect(() => {
     tippy("[data-tippy-content]");
   }, []);
+
+  const handleTabClick = (tab: TabType) => {
+    setActiveTab(tab);
+  };
+
   return (
     <section className="relative py-24 pt-20">
       <picture className="pointer-events-none absolute inset-0 -z-10 dark:hidden">
@@ -27,16 +36,39 @@ export default function Collections() {
           className="nav nav-tabs scrollbar-custom mb-12 flex items-center justify-start overflow-x-auto overflow-y-hidden border-b border-jacarta-100 pb-px dark:border-jacarta-600 md:justify-center"
           role="tablist"
         >
+
           <li className="nav-item" role="presentation">
             <button
-              className="nav-link active relative flex items-center whitespace-nowrap py-3 px-6 text-jacarta-400 hover:text-jacarta-700 dark:hover:text-white"
-              id="owned-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#owned"
-              type="button"
-              role="tab"
-              aria-controls="owned"
-              aria-selected="false"
+              className={`nav-link relative flex items-center whitespace-nowrap py-3 px-6 ${
+                activeTab === "create"
+                  ? "active text-jacarta-700 dark:text-white"
+                  : "text-jacarta-400 hover:text-jacarta-700 dark:hover:text-white"
+              }`}
+              onClick={() => handleTabClick("create")}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                className="mr-1 h-5 w-5 fill-current"
+              >
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path d="M11.95 7.95l-1.414 1.414L8 6.828 8 20H6V6.828L3.465 9.364 2.05 7.95 7 3l4.95 4.95zm10 8.1L17 21l-4.95-4.95 1.414-1.414 2.537 2.536L16 4h2v13.172l2.536-2.536 1.414 1.414z" />
+              </svg>
+              <span className="font-display text-base font-medium">
+                Create NFT Heritage
+              </span>
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+              className={`nav-link relative flex items-center whitespace-nowrap py-3 px-6 ${
+                activeTab === "owned"
+                  ? "active text-jacarta-700 dark:text-white"
+                  : "text-jacarta-400 hover:text-jacarta-700 dark:hover:text-white"
+              }`}
+              onClick={() => handleTabClick("owned")}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -48,19 +80,19 @@ export default function Collections() {
                 <path fill="none" d="M0 0h24v24H0z" />
                 <path d="M12.414 5H21a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h7.414l2 2zM4 5v14h16V7h-8.414l-2-2H4zm9 8h3l-4 4-4-4h3V9h2v4z" />
               </svg>
-              <span className="font-display text-base font-medium">Owned NFT</span>
+              <span className="font-display text-base font-medium">
+                Owned NFT
+              </span>
             </button>
           </li>
           <li className="nav-item" role="presentation">
             <button
-              className="nav-link relative flex items-center whitespace-nowrap py-3 px-6 text-jacarta-400 hover:text-jacarta-700 dark:hover:text-white"
-              id="created-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#created"
-              type="button"
-              role="tab"
-              aria-controls="created"
-              aria-selected="false"
+              className={`nav-link relative flex items-center whitespace-nowrap py-3 px-6 ${
+                activeTab === "herited"
+                  ? "active text-jacarta-700 dark:text-white"
+                  : "text-jacarta-400 hover:text-jacarta-700 dark:hover:text-white"
+              }`}
+              onClick={() => handleTabClick("herited")}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -77,108 +109,96 @@ export default function Collections() {
               </span>
             </button>
           </li>
-          <li className="nav-item" role="presentation">
-            <button
-              className="nav-link relative flex items-center whitespace-nowrap py-3 px-6 text-jacarta-400 hover:text-jacarta-700 dark:hover:text-white"
-              id="activity-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#activity"
-              type="button"
-              role="tab"
-              aria-controls="activity"
-              aria-selected="false"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-                className="mr-1 h-5 w-5 fill-current"
-              >
-                <path fill="none" d="M0 0h24v24H0z" />
-                <path d="M11.95 7.95l-1.414 1.414L8 6.828 8 20H6V6.828L3.465 9.364 2.05 7.95 7 3l4.95 4.95zm10 8.1L17 21l-4.95-4.95 1.414-1.414 2.537 2.536L16 4h2v13.172l2.536-2.536 1.414 1.414z" />
-              </svg>
-              <span className="font-display text-base font-medium">
-                Activity
-              </span>
-            </button>
-          </li>
         </ul>
-        {/* Collections Tab */}
-        <div
-            className="tab-pane fade"
-            id="collections"
-            role="tabpanel"
-            aria-labelledby="collections-tab"
-          >
-            <div className="grid grid-cols-1 gap-[1.875rem] md:grid-cols-2 lg:grid-cols-4">
-              {collections.slice(0, 4).map((elm, i) => (
-                <article key={i}>
-                  <div className="rounded-2.5xl border border-jacarta-100 bg-white p-[1.1875rem] transition-shadow hover:shadow-lg dark:border-jacarta-700 dark:bg-jacarta-700">
-                    <Link
-                      href={`/collection/${elm.id}`}
-                      className="flex space-x-[0.625rem]"
-                    >
-                      <span className="w-[74.5%]">
-                        <Image
-                          width={152}
-                          height={242}
-                          src={elm.images[0]}
-                          alt="item 1"
-                          className="h-full w-full rounded-[0.625rem] object-cover"
-                          loading="lazy"
-                        />
-                      </span>
-                      <span className="flex w-1/3 flex-col space-y-[0.625rem]">
-                        {elm.images.slice(1).map((img, i2) => (
+
+        {/* Tab Content */}
+        <div className="tab-content">
+          {activeTab === "owned" && (
+            <div className="tab-pane fade show active" role="tabpanel">
+              <div className="grid grid-cols-1 gap-[1.875rem] md:grid-cols-2 lg:grid-cols-4">
+                {collections.slice(0, 4).map((elm, i) => (
+                  <article key={i}>
+                    <div className="rounded-2.5xl border border-jacarta-100 bg-white p-[1.1875rem] transition-shadow hover:shadow-lg dark:border-jacarta-700 dark:bg-jacarta-700">
+                      <Link
+                        href={`/collection/${elm.id}`}
+                        className="flex space-x-[0.625rem]"
+                      >
+                        <span className="w-[74.5%]">
                           <Image
-                            width={68}
-                            height={74}
-                            key={i2}
-                            src={img}
+                            width={152}
+                            height={242}
+                            src={elm.images[0]}
                             alt="item 1"
-                            className="h-full rounded-[0.625rem] object-cover"
+                            className="h-full w-full rounded-[0.625rem] object-cover"
                             loading="lazy"
                           />
-                        ))}
-                      </span>
-                    </Link>
+                        </span>
+                        <span className="flex w-1/3 flex-col space-y-[0.625rem]">
+                          {elm.images.slice(1).map((img, i2) => (
+                            <Image
+                              width={68}
+                              height={74}
+                              key={i2}
+                              src={img}
+                              alt="item 1"
+                              className="h-full rounded-[0.625rem] object-cover"
+                              loading="lazy"
+                            />
+                          ))}
+                        </span>
+                      </Link>
 
-                    <Link
-                      href={`/collection/${elm.id}`}
-                      className="mt-4 block font-display text-base text-jacarta-700 hover:text-accent dark:text-white dark:hover:text-accent"
-                    >
-                      {elm.name}
-                    </Link>
+                      <Link
+                        href={`/collection/${elm.id}`}
+                        className="mt-4 block font-display text-base text-jacarta-700 hover:text-accent dark:text-white dark:hover:text-accent"
+                      >
+                        {elm.name}
+                      </Link>
 
-                    <div className="mt-2 flex items-center justify-between text-sm font-medium tracking-tight">
-                      <div className="flex flex-wrap items-center">
-                        <Link
-                          href={`/user/${elm.id}`}
-                          className="mr-2 shrink-0"
-                        >
-                          <Image
-                            width={20}
-                            height={20}
-                            src={elm.avatar}
-                            alt="owner"
-                            className="h-5 w-5 rounded-full"
-                          />
-                        </Link>
-                        <span className="mr-1 dark:text-jacarta-400">by</span>
-                        <Link href={`/user/${elm.id}`} className="text-accent">
-                          <span>{elm.ownerName}</span>
-                        </Link>
+                      <div className="mt-2 flex items-center justify-between text-sm font-medium tracking-tight">
+                        <div className="flex flex-wrap items-center">
+                          <Link
+                            href={`/user/${elm.id}`}
+                            className="mr-2 shrink-0"
+                          >
+                            <Image
+                              width={20}
+                              height={20}
+                              src={elm.avatar}
+                              alt="owner"
+                              className="h-5 w-5 rounded-full"
+                            />
+                          </Link>
+                          <span className="mr-1 dark:text-jacarta-400">by</span>
+                          <Link
+                            href={`/user/${elm.id}`}
+                            className="text-accent"
+                          >
+                            <span>{elm.ownerName}</span>
+                          </Link>
+                        </div>
+                        <span className="text-sm dark:text-jacarta-300">
+                          {elm.itemCount} Items
+                        </span>
                       </div>
-                      <span className="text-sm dark:text-jacarta-300">
-                        {elm.itemCount} Items
-                      </span>
                     </div>
-                  </div>
-                </article>
-              ))}
+                  </article>
+                ))}
+              </div>
             </div>
-          </div>
+          )}
+          {activeTab === "herited" && (
+            <div className="tab-pane fade show active" role="tabpanel">
+              <h2>Herited NFT Content</h2>
+              {/* Add your herited NFT content here */}
+            </div>
+          )}
+          {activeTab === "create" && (
+            <div className="tab-pane fade show active" role="tabpanel">
+              <CreateAction />
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
